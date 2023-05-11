6bad12786ae9a94466ed9d08c0c522c6d3c59f70
oetima01 <107166694+oetima01@users.noreply.github.com>
2023-05-11 15:30:07 +0400

    p

---
+++ food/back/bluda/bluda/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bluda project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bluda.settings')
+
+application = get_asgi_application()
